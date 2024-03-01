3a5b8d03ba0b50f0bc418d79d363272569a2c7b8
i528376 <v.som.sekhar@sap.com>
2024-03-01 20:36:40 +0530

    valid palindrome

---
+++ LeetCode/Step 1 : Learn the basics/Know Basic Maths/ValidPalindrome.java
@@ -0,0 +1,26 @@
+class ValidPalindrome {
+
+    public static void main(String[] args) {
+		System.out.println(isPalindrome("test"));
+	}
+
+    public static boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                ++i;
+            } else if (!Character.isLetterOrDigit(s.charAt(j))) {
+                --j;
+            } else if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            } else {
+                ++i;
+                --j;
+            }
+        }
+        return true;
+    }
+}
+
+
+// https://leetcode.com/problems/valid-palindrome/
